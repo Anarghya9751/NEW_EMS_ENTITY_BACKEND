a751690bb1b278b1fd5bc953811cc2bb3f64dcd5
vijeth6 <g.bharath@anarghyacomm.in>
2024-05-28 17:03:48 +0530

    first commit

---
+++ src/main/java/com/ems/NewEmsEntityBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewEmsEntityBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewEmsEntityBackendApplication.class, args);
+	}
+
+}
